904ca5b8ec88acd187476c9b466e1f5a79035803
david-guerra <davidguerras2903@gmail.com>
2026-07-18 22:12:45 +0200

    feat: add accessible contact dialog

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ContactProvider from './features/contact/ContactProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <ContactProvider>
+            <App />
+        </ContactProvider>
     </StrictMode>,
 )
